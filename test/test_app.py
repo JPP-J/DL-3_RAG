3c9db4a6e5952319ea0c60bdd712bdad1ec3858a
JPP-J <jidapa.cmp63@gmail.com>
2025-06-06 12:29:44 +0700

    update test

--- test/test_app.py
+++ test/test_app.py
@@ -1,12 +1,13 @@
 from unittest.mock import patch
 from app.utils.ollama import response_answer_gr
 
-@patch("utils.ollama.stream_ollama_response")
-def test_response_answer_gr_mocked(mock_stream):
-    mock_stream.return_value = iter(["This is a mock response."])
 
+from unittest.mock import patch
+
+@patch("utils.ollama.stream_ollama_response")
+@patch("utils.rag_pipeline.load_folder_documents", return_value=["Fake doc"])
+def test_response_answer_gr_mocked(mock_docs, mock_stream):
+    mock_stream.return_value = iter(["This is a mock response."])
     query = "What is AI?"
     result = "".join(response_answer_gr(query))
-
     assert "mock response" in result
-
